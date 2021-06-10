1fcb0557c7e4d7ff84cbcc04e43d8426f3ac47c0
Orcha02 <orcha2002@gmail.com>
2021-06-10 23:03:01 +0000

    Add Readme

---
+++ 0-bubble_sort.c
@@ -0,0 +1,7 @@
+/**
+ * bubble_sort - Sorts an array of integers with bubble sort algorithm
+ * @array: Array of numbers that's going to be sorted
+ * @size: Size of array
+ * Return: Nothing, will sort and print when swap
+ */
+void bubble_sort(int *array, size_t size)
