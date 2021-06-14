3f85ce278a6492a6e3b1544302a56ef8e1692a67
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-06-14 17:55:42 +0000

    fix 3 NULL case after restore

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,10 +13,12 @@ void print_quick_sort(int *array, size_t size, int *o_array, size_t o_size);
 void print_quick_sort(int *array, size_t size, int *o_array, size_t o_size)
 {
 	unsigned int i = 0, j = 0;
-	int p = array[size - 1], n;
+	int p, n;
 
-	if (size <= 1)
+	if (!array || size <= 1)
 		return;
+
+	p = array[size - 1];
 
 	for (j = 0; j < size; j++)
 		if (array[j] <= p || j == size - 1)
