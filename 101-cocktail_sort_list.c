c8deebf3b6609bf5e62c6f04d1ec4dabce3a1fbb
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-06-14 22:04:24 +0000

    fix 101 case order decresing

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -17,31 +17,30 @@ void cocktail_sort_list(listint_t **list)
 		c = start ? start->next : *list;
 
 		while (c && c->next && c->next != end)
-		{
 			if (c->n > c->next->n)
 			{
 				swap(c, c->next);
-				c = c->prev;
 				swaped = 1;
+				if (c->prev && !c->prev->prev)
+					*list = c->prev;
 				print_list(*list);
 			}
-			c = c->next;
-		}
+			else
+				c = c->next;
 		end = c;
 		c = c->prev;
+
 		while (c && c->prev && c->prev != start)
-		{
 			if (c->n < c->prev->n)
 			{
 				swap(c->prev, c);
 				swaped = 1;
 				if (!c->prev)
 					*list = c;
-				c = c->next;
 				print_list(*list);
 			}
-			c = c->prev;
-		}
+			else
+				c = c->prev;
 		start = c;
 	}
 }
@@ -52,12 +51,12 @@ void cocktail_sort_list(listint_t **list)
  */
 void swap(listint_t *n1, listint_t *n2)
 {
-        n1->next = n2->next;
-        if (n2->next)
-                n2->next->prev = n1;
-        n2->next = n1;
-        n2->prev = n1->prev;
-        if (n2->prev)
-                n2->prev->next = n2;
-        n1->prev = n2;
+	n1->next = n2->next;
+	if (n2->next)
+		n2->next->prev = n1;
+	n2->next = n1;
+	n2->prev = n1->prev;
+	if (n2->prev)
+		n2->prev->next = n2;
+	n1->prev = n2;
 }
